71427c66bc7db01157c30eb7824c86af9fd93de9
simraan18 <simraan.khan@evolvextechnology.com>
2026-07-07 11:59:42 +0400

    refactor: config run redis in dev env

--- src/lib/redis/redis.provider.ts
+++ src/lib/redis/redis.provider.ts
@@ -3,6 +3,12 @@ import { Redis } from 'ioredis';
 export const RedisProvider = {
   provide: 'REDIS_CLIENT',
   useFactory: () => {
+    if (process.env.NODE_ENV === 'development') {
+      return new Redis({
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+      });
+    }
     return new Redis(process.env.REDIS_URL!);
   },
 };
